c7196746f1c6c95c3c9018a9ddb0093ce97620b2
edcjian <2015563489@qq.com>
2021-02-06 11:31:52 +0800

    react hooks test v2

--- src/test.js
+++ src/test.js
@@ -1,10 +1,17 @@
-import React from 'react';
-import { useState,useEffect } from 'react';
+import React, {useContext} from 'react';
+import { useState,useEffect,createContext } from 'react';
 import { BrowserRouter as Router ,Route,Link } from 'react-router-dom';
 
 import Index from "./components";
 import List from "./components/list";
 function Test(props) {
+    const countContext=createContext();
+    function Counter() {
+
+        const count = useContext(countContext);
+        return(<h2>{count}</h2>)
+    }
+
     useEffect(() => {
 console.log('you clicked ${count} times')
 
@@ -23,6 +30,7 @@ console.log('you clicked ${count} times')
                     <li><Link to="/">列表</Link></li>
                     <li><Link to="/list/">首页</Link></li>
                 </ul>
+                <countContext.Provider value={count}></countContext.Provider>
                 <Route path="/" exact component={Index}></Route>
                 <Route path="/list" exact component={List}></Route>
             </Router>
